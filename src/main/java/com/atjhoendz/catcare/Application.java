5f1fec813f8c89375e6164960a4740b915fdb381
atjhoendz <achunarmando@gmail>
2018-12-06 20:13:38 +0700

    implementasi reply message

--- src/main/java/com/atjhoendz/catcare/Application.java
+++ src/main/java/com/atjhoendz/catcare/Application.java
@@ -2,10 +2,15 @@ package com.atjhoendz.catcare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
-
+public class Application extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app){
+		return app.sources(Application.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
