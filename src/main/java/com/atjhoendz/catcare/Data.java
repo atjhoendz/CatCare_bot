a785a4b370d72dc4e4eabf8c50e1409ee93fb03f
atjhoendz <achunarmando@gmail>
2018-12-14 01:21:20 +0700

    test logic

--- src/main/java/com/atjhoendz/catcare/Data.java
+++ src/main/java/com/atjhoendz/catcare/Data.java
@@ -8,20 +8,22 @@ import java.util.Set;
 public class Data {
     public String listKeluhan[] = {"nafsu", "berkurang", "bersin", "demam", "batuk", "mata", "berair"};
     public String flu[]={"nafsu", "makan", "berkurang", "demam", "bersin", "batuk"};
-    Set<String> dataKeluhan = new HashSet<String>();
-    Set<String> keluhanUser = new HashSet<String>();
+    Set<String> dataKeluhan;
+    Set<String> keluhanUser;
 
     public Data(){
+        dataKeluhan = new HashSet<String>();
         dataKeluhan.addAll(Arrays.asList(flu));
     }
 
     public String cekKeluhan(ArrayList<String> keluhan){
+        keluhanUser = new HashSet<String>();
         keluhanUser.addAll(keluhan);
 
         Set<String> unionList = new HashSet<String>(dataKeluhan);
         unionList.retainAll(keluhanUser);
 
-        if(unionList.size() == 0){
+        if(unionList.size() > 3){
             return "Flu";
         }else{
             return "Sehat";
