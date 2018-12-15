4ec7e6d481131f7c97f58886e450f6fbbc5c2d2a
atjhoendz <achunarmando@gmail>
2018-12-15 21:58:42 +0700

    add list penyakit

--- src/main/java/com/atjhoendz/catcare/DataKeluhan.java
+++ src/main/java/com/atjhoendz/catcare/DataKeluhan.java
@@ -7,6 +7,14 @@ public class DataKeluhan {
     private String flu[]={"nafsu", "makan", "berkurang", "demam", "bersin", "batuk"};
     private String cacingan = "kotoran muntah cacing cacingnya tidak nafsu perut membuncit wajar badan kurus haus diare mencret makan banyak tapi badan tetap kurus";
     private String arrCacingan[] = cacingan.split(" ");
+    private String koksidiosis[] = {"malas", "gerak", "bergerak", "mager", "anemia", "kekurangan", "kurang", "darah", "diare"};
+    private String tripanosomiasis[] = {"malas", "gerak", "bergerak", "mager", "anemia", "kekurangan", "kurang", "darah", "anoreksia", "gamau", "makan", "epifora", "mata", "berair"};
+    private String hepatozoonosis[] = {"malas", "gerak", "bergerak", "mager", "demam", "pucat"};
+    private String babesiosis[] = {"kekurusan", "badan", "kurus", "kurusan", "malas", "gerak", "bergerak", "mager", "demam", "anoreksia", "gamau", "makan", "kelumpuhan", "pincang"};
+    private String distemper[] = {"dehidrasi", "malas", "gerak", "bergerak", "mager", "diare", "demam", "anoreksia", "gamau", "makan", "menurun", "halitosis", "bau", "mulut", "muntah", "lesi anggota"};
+    private String ankilostomiosis[] = {"anemia", "kekurangan", "kurang", "darah", "diare", "berak", "darah", "melena", "ikterus", "kulit", "kuning", "lesi", "anggota", "batuk", "radang", "kulit"};
+    private String phthiriasis[] = {"kekurusan", "kurusan", "badan", "kurus", "anemia", "kekurangan", "kurang", "darah", "ikterus", "kulit", "kuning", "radang", "kulit"};
+    private String pedikulosis[] = {"kutu", "kutuan", "radang", "kulit"};
 
     Map<String, List<String>> dataPenyakit = new HashMap<String, List<String>>();
     private String hasilPenyakit = "Sehat";
@@ -14,8 +22,25 @@ public class DataKeluhan {
     public DataKeluhan(){
         List<String> kCacingan = new ArrayList<String>(Arrays.asList(arrCacingan));
         List<String> kFlu = new ArrayList<>(Arrays.asList(flu));
+        List<String> kKoks = new ArrayList<>(Arrays.asList(koksidiosis));
+        List<String> kTrip = new ArrayList<>(Arrays.asList(tripanosomiasis));
+        List<String> kHepa = new ArrayList<>(Arrays.asList(hepatozoonosis));
+        List<String> kBabe = new ArrayList<>(Arrays.asList(babesiosis));
+        List<String> kDist = new ArrayList<>(Arrays.asList(distemper));
+        List<String> kAnki = new ArrayList<>(Arrays.asList(ankilostomiosis));
+        List<String> kPhth = new ArrayList<>(Arrays.asList(phthiriasis));
+        List<String> kPedi = new ArrayList<>(Arrays.asList(pedikulosis));
+
         dataPenyakit.put("Flu", kFlu);
         dataPenyakit.put("Cacingan", kCacingan);
+        dataPenyakit.put("Koksidiosis", kKoks);
+        dataPenyakit.put("Tripanosomiasis", kTrip);
+        dataPenyakit.put("Hepatozoonosis", kHepa);
+        dataPenyakit.put("Babesiosis", kBabe);
+        dataPenyakit.put("Distemper", kDist);
+        dataPenyakit.put("Ankilostomiosis", kAnki);
+        dataPenyakit.put("Phthiriasis", kPhth);
+        dataPenyakit.put("Pedikulosis", kPedi);
     }
 
     public String cekKeluhan(ArrayList<String> keluhan){
@@ -37,5 +62,13 @@ public class DataKeluhan {
         }
 
         return this.hasilPenyakit;
+    }
+
+    public List<String> getPenyakit(DataKeluhan data){
+        List<String> namaPenyakit = new ArrayList<String>();
+        for(Map.Entry<String, List<String>> entry : data.dataPenyakit.entrySet()){
+            namaPenyakit.add(entry.getKey());
+        }
+        return namaPenyakit;
     }
 }
--- src/main/java/com/atjhoendz/catcare/LineBotController.java
+++ src/main/java/com/atjhoendz/catcare/LineBotController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -23,6 +24,7 @@ import java.util.concurrent.ExecutionException;
 public class LineBotController {
 
     ArrayList<String> keluhanUser = new ArrayList<String>();
+    List<String> listPenyakit = new ArrayList<String>();
     DataKeluhan data = new DataKeluhan();
     DataMessage jawabanMasuk = new DataMessage();
     private String state = "";
@@ -67,6 +69,7 @@ public class LineBotController {
             String msgText = "";
             String userId = payload.events[0].source.userId;
             String eventType = payload.events[0].type;
+            listPenyakit = data.getPenyakit(data);
 
 
             if(eventType.equals("follow")){
@@ -91,11 +94,11 @@ public class LineBotController {
                     keluhanUser.addAll(Arrays.asList(arrMsg));
                     replyToUser(payload.events[0].replyToken, "Ada keluhan lagi?");
                 }else if(ans.equals("tidak") && state.equals("")){
-                    replyToUser(payload.events[0].replyToken, "Selamat kucing anda baik baik saja :)");
+                    replyToUser(payload.events[0].replyToken, "Selamat kucing anda baik baik saja :)\n\nOptions :\n- help/h/bantuan = menampilkan bantuan.\n- care = Untuk memulai konsultasi dengan CatCare.\n- list = Untuk melihat data penyakit yang ada di CatCare.");
                 }else if(ans.equals("tidak") && state.equals("adakeluhan")){
                     String hasil = data.cekKeluhan(keluhanUser);
                     if(hasil.equals("unknown")){
-                        replyToUser(payload.events[0].replyToken, "Mohon maaf kak, Belum ada data yang cocok terhadap keluhan-keluhan tersebut :)");
+                        replyToUser(payload.events[0].replyToken, "Mohon maaf kak, Belum ada penyakit yang cocok terhadap keluhan-keluhan tersebut, mungkin itu hanya keluhan normal");
                         state = "";
                     }else if(hasil.equals("Sehat")){
                         replyToUser(payload.events[0].replyToken, "Kucing anda sehat, itu hanya keluhan normal");
@@ -106,32 +109,34 @@ public class LineBotController {
                     }
                     keluhanUser.clear();
                 }else if(ans.equals("tidakSopan")){
-                    replyToUser(payload.events[0].replyToken, "Tidak sopan kamu ferguso, dasar " + msgText + "\n\nOptions :\n- help/h/bantuan = menampilkan bantuan.\n- care = Untuk memulai konsultasi dengan CatCare.");
+                    replyToUser(payload.events[0].replyToken, "Tidak sopan kamu ferguso, dasar " + msgText + "\n\nOptions :\n- help/h/bantuan = menampilkan bantuan.\n- care = Untuk memulai konsultasi dengan CatCare.\n- list = Untuk melihat data penyakit yang ada di CatCare.");
                     state = "";
                 }else if(ans.equals("thanks")){
-                    replyToUser(payload.events[0].replyToken, "Sama sama kak :)\n\n Semoga lekas sembuh kucingnyaa "+ emojiLove +"\n\nOptions :\n- help/h/bantuan = menampilkan bantuan.\n- care = Untuk memulai konsultasi dengan CatCare.");
+                    replyToUser(payload.events[0].replyToken, "Sama sama kak :)\n\n Semoga lekas sembuh kucingnyaa "+ emojiLove +"\n\nOptions :\n- help/h/bantuan = menampilkan bantuan.\n- care = Untuk memulai konsultasi dengan CatCare.\n- list = Untuk melihat data penyakit yang ada di CatCare.");
                     state = "";
                 }else if(ans.equals("sapa")){
                     replyToUser(payload.events[0].replyToken, "Halo " + namaUser + ", \nApakah kucing anda memiliki keluhan?");
                     state = "";
                 }else if(ans.equals("unknown")){
-                    replyToUser(payload.events[0].replyToken, "Haii silahkan pilih opsinya kak :)\n\nOptions :\n- help/h/bantuan = menampilkan bantuan.\n- care = Untuk memulai konsultasi dengan CatCare.");
+                    replyToUser(payload.events[0].replyToken, "Haii silahkan pilih opsinya kak :)\n\nOptions :\n- help/h/bantuan = menampilkan bantuan.\n- care = Untuk memulai konsultasi dengan CatCare.\n- list = Untuk melihat data penyakit yang ada di CatCare.");
                     state = "";
                 }else if(ans.equals("bingung")){
                     replyToUser(payload.events[0].replyToken, "Iya seperti itu kak,\nApakah kucing anda memiliki keluhan?");
                     state = "";
                 }else if(ans.equals("author")){
-                    replyToUser(payload.events[0].replyToken, "Pembuat saya adalah \n Mohamad Achun Armando (140810170020) \n\n Data kucing dari \n Refa Annisatul Ilma (140810170060).\n\nOptions :\n- help/h/bantuan = menampilkan bantuan.\n- care = Untuk memulai konsultasi dengan CatCare.");
+                    replyToUser(payload.events[0].replyToken, "Pembuat saya adalah \n Mohamad Achun Armando (140810170020) \n\n Data kucing dari \n Refa Annisatul Ilma (140810170060).\n\nOptions :\n- help/h/bantuan = menampilkan bantuan.\n- care = Untuk memulai konsultasi dengan CatCare.\n- list = Untuk melihat data penyakit yang ada di CatCare.");
                     state = "";
                 }else if(ans.equals("myname")){
-                    replyToUser(payload.events[0].replyToken, "Dia adalah pembuat saya bernama lengkap Mohamad Achun Armando.\n\nOptions :\n- help/h/bantuan = menampilkan bantuan.\n- care = Untuk memulai konsultasi dengan CatCare.");
+                    replyToUser(payload.events[0].replyToken, "Dia adalah pembuat saya bernama lengkap Mohamad Achun Armando.\n\nOptions :\n- help/h/bantuan = menampilkan bantuan.\n- care = Untuk memulai konsultasi dengan CatCare.\n- list = Untuk melihat data penyakit yang ada di CatCare.");
                     state = "";
                 }else if(ans.equals("help")){
-                    replyToUser(payload.events[0].replyToken, "Selamat Datang di CatCare,\nCatCare adalah bot untuk mengetahui penyakit dari keluhan-keluhan yang dialami oleh kucing kesayanganmu.\n\nOptions :\n- help/h/bantuan = menampilkan bantuan.\n- care = Untuk memulai konsultasi dengan CatCare.\n\n*Note : Untuk mengidentifikasi masing masing penyakitnya bot ini membutuhkan lebih dari 3 keluhan yang cocok.\n\nBot ini dibuat untuk memenuhi project tugas akhir mata kuliah Pemrograman Berorientasi Objek. Mohon maaf apabila hasil konsultasi yang kurang maksimal atau tidak sesuai itu dikarenakan minimnya data yang kita miliki.\n\nAnggota Kelompok : \nMohamad Achun Armando (140810170020)\nRefa Annisatul Ilma (140810170060)\n\nTeknik Informatika '17\nUniversitas Padjadjaran");
+                    replyToUser(payload.events[0].replyToken, "Selamat Datang di CatCare,\nCatCare adalah bot untuk mengetahui penyakit dari keluhan-keluhan yang dialami oleh kucing kesayanganmu.\n\nOptions :\n- help/h/bantuan = menampilkan bantuan.\n- care = Untuk memulai konsultasi dengan CatCare.\n- list = Untuk melihat data penyakit yang ada di CatCare.\n\n*Note : Untuk mengidentifikasi masing masing penyakitnya bot ini membutuhkan lebih dari 3 keluhan yang cocok.\n\nBot ini dibuat untuk memenuhi project tugas akhir mata kuliah Pemrograman Berorientasi Objek. Mohon maaf apabila hasil konsultasi yang kurang maksimal atau tidak sesuai itu dikarenakan minimnya data yang kita miliki.\n\nAnggota Kelompok : \nMohamad Achun Armando (140810170020)\nRefa Annisatul Ilma (140810170060)\n\nTeknik Informatika '17\nUniversitas Padjadjaran");
                     state = "";
+                }else if(msgText.equals("list")){
+                    replyToUser(payload.events[0].replyToken, "List Penyakit : \n"+listPenyakit.toString());
                 }
                 else{
-                    replyToUser(payload.events[0].replyToken, "Haii silahkan pilih opsinya kak :)\n\nOptions :\n- help/h/bantuan = menampilkan bantuan.\n- care = Untuk memulai konsultasi dengan CatCare.");
+                    replyToUser(payload.events[0].replyToken, "Haii silahkan pilih opsinya kak :)\n\nOptions :\n- help/h/bantuan = menampilkan bantuan.\n- care = Untuk memulai konsultasi dengan CatCare.\n- list = Untuk melihat data penyakit yang ada di CatCare.");
                 }
             }
 
